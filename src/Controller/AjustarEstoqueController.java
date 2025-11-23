c5c4f5ce886074e8d79a5b4bfebc992aee00b648
Matheus <joaosobramatheus@gmail.com>
2025-11-23 19:52:10 -0300

    <feat and style> Adiciona visual em AjustarEstoque e o controller do mesmo

---
+++ src/Controller/AjustarEstoqueController.java
@@ -0,0 +1,36 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AjustarEstoqueController {
+
+    @FXML
+    private ComboBox<?> comboOperacao;
+
+    @FXML
+    private Label lblEstoqueAtual;
+
+    @FXML
+    private Label lblNomePeca;
+
+    @FXML
+    private Label lblNovoEstoque;
+
+    @FXML
+    private TextField txtQuantidade;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmar(ActionEvent event) {
+
+    }
+
+}
